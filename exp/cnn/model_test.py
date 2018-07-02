fa4901ad0be45db52d18e4d264da5dbc6b98dc05
Lijie Huang <huanglijie@outlook.com>
2018-07-02 19:49:33 +0800

    update

---
+++ exp/cnn/model_test.py
@@ -0,0 +1,60 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import tensorflow as tf
+import sys
+
+sys.path.append('../')
+import tf_util
+
+def get_model(input_images, is_training, cat_num,  weight_decay, bn_decay):
+    input_data = tf.expand_dims(input_images, -1)
+
+    net = tf_util.conv2d(input_data, 64, [3, 3], stride=[1, 1], padding='SAME',
+                         scope='conv1', weight_decay=weight_decay,
+                         bn_decay=bn_decay, bn=True, is_training=is_training)
+    net = tf_util.max_pool2d(net, [2, 2], stride=[2, 2], scope='mp1',
+                             padding='SAME')
+
+    net = tf_util.conv2d(net, 128, [5, 5], stride=[1, 1], padding='SAME',
+                         scope='conv2', weight_decay=weight_decay,
+                         bn_decay=bn_decay, bn=True, is_training=is_training)
+    net = tf_util.max_pool2d(net, [2, 2], stride=[2, 2], scope='mp2',
+                             padding='SAME')
+
+    net = tf_util.conv2d(net, 512, [3, 3], stride=[1, 1], padding='SAME',
+                         scope='conv3', weight_decay=weight_decay,
+                         bn_decay=bn_decay, bn=True, is_training=is_training)
+    net = tf_util.max_pool2d(net, [2, 2], stride=[2, 2], scope='mp3',
+                             padding='SAME')
+
+    net = tf_util.conv2d(net, 512, [3, 3], stride=[1, 1], padding='SAME',
+                         scope='conv4', weight_decay=weight_decay,
+                         bn_decay=bn_decay, bn=True, is_training=is_training)
+    net = tf_util.max_pool2d(net, [2, 2], stride=[2, 2], scope='mp4',
+                             padding='SAME')
+    
+    conv_out_shape = net.get_shape()
+    reduce_shape = conv_out_shape[1]*conv_out_shape[2]
+    net = tf.reshape(net, [-1, reduce_shape, 512])
+    net = tf.reduce_mean(net, axis=1)
+
+    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
+                                  weight_decay=weight_decay, bn_decay=bn_decay,
+                                  scope='fc1')
+    net = tf_util.dropout(net, is_training=is_training, keep_prob=0.5,
+                          scope='dp1')
+
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
+                                  weight_decay=weight_decay, bn_decay=bn_decay,
+                                  scope='fc2')
+    net = tf_util.dropout(net, is_training=is_training, keep_prob=0.5,
+                          scope='dp2')
+
+    net = tf_util.fully_connected(net, cat_num, bn=False,
+                                  is_training=is_training,
+                                  weight_decay=weight_decay,
+                                  activation_fn=None, scope='fc3')
+
+    return net
+
